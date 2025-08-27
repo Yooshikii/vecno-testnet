ffd2fd4cc629238254a385ec9b0c1bc2100fbe21
Yooshikii <knut-a_91@hotmail.com>
2025-08-27 21:36:33 +0200

    Updated wasm.

--- consensus/pow/src/wasm.rs
+++ consensus/pow/src/wasm.rs
@@ -1,14 +1,11 @@
-use crate::matrix::Matrix;
+use crate::State;
 use js_sys::BigInt;
-use num::Float;
 use vecno_consensus_client::Header;
 use vecno_consensus_client::HeaderT;
 use vecno_consensus_core::hashing;
-use vecno_hashes::Hash;
-use vecno_hashes::PowHash;
+use vecno_hashes::{Hash, PowHash};
 use vecno_math::Uint256;
-use vecno_utils::hex::FromHex;
-use vecno_utils::hex::ToHex;
+use vecno_utils::hex::{FromHex, ToHex};
 use wasm_bindgen::prelude::*;
 use workflow_wasm::convert::TryCastFromJs;
 use workflow_wasm::error::Error;
@@ -24,7 +21,7 @@ extern "C" {
 /// @category Mining
 #[wasm_bindgen(inspectable)]
 pub struct PoW {
-    inner: crate::State,
+    inner: State,
     pre_pow_hash: Hash,
 }
 
@@ -32,32 +29,35 @@ pub struct PoW {
 impl PoW {
     #[wasm_bindgen(constructor)]
     pub fn new(header: &HeaderT, timestamp: Option<u64>) -> Result<PoW> {
-        // this function replicates crate::State::new() but caches
-        // the pre_pow_hash value internally, making it available
-        // via the `pre_pow_hash` property getter.
         let header = Header::try_cast_from(header).map_err(Error::custom)?;
         let header = header.as_ref();
         let header = header.inner();
 
-        // Get required target from header bits.
+        // Get required target from header bits
         let target = Uint256::from_compact_target_bits(header.bits);
-        // Zero out the time and nonce.
+        // Calculate pre_pow_hash by zeroing out nonce and time
         let pre_pow_hash = hashing::header::hash_override_nonce_time(header, 0, 0);
-        // PRE_POW_HASH || TIME || 32 zero byte padding || NONCE
+        // Use the provided timestamp or fallback to header timestamp
         let hasher = PowHash::new(pre_pow_hash, timestamp.unwrap_or(header.timestamp));
-        let matrix = Matrix::generate(pre_pow_hash);
 
-        Ok(Self { inner: crate::State { matrix, target, hasher }, pre_pow_hash })
+        Ok(Self {
+            inner: State {
+                target,
+                hasher,
+                timestamp: timestamp.unwrap_or(header.timestamp),
+            },
+            pre_pow_hash,
+        })
     }
 
-    /// The target based on the provided bits.
+    /// The target based on the provided bits
     #[wasm_bindgen(getter)]
     pub fn target(&self) -> Result<BigInt> {
         self.inner.target.try_into().map_err(|err| Error::custom(format!("{err:?}")))
     }
 
-    /// Checks if the computed target meets or exceeds the difficulty specified in the template.
-    /// @returns A boolean indicating if it reached the target and a bigint representing the reached target.
+    /// Checks if the computed target meets or exceeds the difficulty specified in the template
+    /// @returns A boolean indicating if it reached the target and a bigint representing the reached target
     #[wasm_bindgen(js_name=checkWork)]
     pub fn check_work(&self, nonce: u64) -> Result<WorkT> {
         let (c, v) = self.inner.check_pow(nonce);
@@ -68,34 +68,37 @@ impl PoW {
         Ok(array.unchecked_into())
     }
 
-    /// Hash of the header without timestamp and nonce.
+    /// Hash of the header without timestamp and nonce
     #[wasm_bindgen(getter = prePoWHash)]
     pub fn get_pre_pow_hash(&self) -> String {
         self.pre_pow_hash.to_hex()
     }
 
-    /// Can be used for parsing Stratum templates.
+    /// Can be used for parsing Stratum templates
     #[wasm_bindgen(js_name=fromRaw)]
     pub fn from_raw(pre_pow_hash: &str, timestamp: u64, target_bits: Option<u32>) -> Result<PoW> {
         // Convert the pre_pow_hash from hex string to Hash
         let pre_pow_hash = Hash::from_hex(pre_pow_hash).map_err(|err| Error::custom(format!("{err:?}")))?;
-
         // Generate the target from compact target bits if provided
         let target = Uint256::from_compact_target_bits(target_bits.unwrap_or_default());
-
-        // Initialize the matrix and hasher using pre_pow_hash and timestamp
-        let matrix = Matrix::generate(pre_pow_hash);
+        // Initialize the hasher using pre_pow_hash and timestamp
         let hasher = PowHash::new(pre_pow_hash, timestamp);
 
-        Ok(PoW { inner: crate::State { matrix, target, hasher }, pre_pow_hash })
+        Ok(PoW {
+            inner: State {
+                target,
+                hasher,
+                timestamp,
+            },
+            pre_pow_hash,
+        })
     }
 }
 
 // https://github.com/tmrlvi/vecno-miner/blob/bf361d02a46c580f55f46b5dfa773477634a5753/src/client/stratum.rs#L36
 const DIFFICULTY_1_TARGET: (u64, i16) = (0xffffu64, 208); // 0xffff 2^208
 
-/// Calculates target from difficulty, based on set_difficulty function on
-/// <https://github.com/tmrlvi/vecno-miner/blob/bf361d02a46c580f55f46b5dfa773477634a5753/src/client/stratum.rs#L375>
+/// Calculates target from difficulty, based on set_difficulty function
 /// @category Mining
 #[wasm_bindgen(js_name = calculateTarget)]
 pub fn calculate_target(difficulty: f32) -> Result<BigInt> {
